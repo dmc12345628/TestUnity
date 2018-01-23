037163092fca221e1d2fd64eff94ba46c97f84f0
Jesus Daniel Medina Cruz <dmc12345628@gmail.com>
2018-01-23 18:24:20 +0100

    MainMenu and was ended with all animations. PersonalDataMenu was added with Crapoto and Peluchon animated. Sound game was added.

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour {
+
+    public enum MenuStates { Main, PersonalData, Companions }
+    public MenuStates currenteState;
+
+    public GameObject mainMenu;
+    public GameObject personalDataMenu;
+
+	// Use this for initialization
+	void Start () {
+        currenteState = MenuStates.Main;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mainMenu.SetActive(currenteState == MenuStates.Main);
+        personalDataMenu.SetActive(currenteState == MenuStates.PersonalData);
+    }
+
+    public void OnPreparerParcours()
+    {
+        Debug.Log("On preparer parcours");
+        currenteState = MenuStates.PersonalData;
+    }
+}
