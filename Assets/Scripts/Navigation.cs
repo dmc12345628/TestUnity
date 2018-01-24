b8a6d79f4d83a04a7d813a1511e0ce357dfc5fe6
Jesus Daniel Medina Cruz <dmc12345628@gmail.com>
2018-01-24 11:10:04 +0100

    btnBackMainMenu and btnValiderPersonalData were added in PersonalDataMenu

--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnClick : MonoBehaviour {
+public class Navigation : MonoBehaviour {
 
-    public enum MenuStates { Main, PersonalData, Companions }
+    public enum MenuStates
+    {
+        Main,
+        PersonalData,
+        Companions
+    }
     public MenuStates currenteState;
 
     public GameObject mainMenu;
     public GameObject personalDataMenu;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         currenteState = MenuStates.Main;
 	}
 	
@@ -24,7 +30,11 @@ public class OnClick : MonoBehaviour {
 
     public void OnPreparerParcours()
     {
-        Debug.Log("On preparer parcours");
         currenteState = MenuStates.PersonalData;
+    }
+
+    public void OnBackMainMenu()
+    {
+        currenteState = MenuStates.Main;
     }
 }
