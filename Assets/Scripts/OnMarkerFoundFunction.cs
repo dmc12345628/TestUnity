8034eea92f730266d8d1cb01c05642061773a9f4
JosselinBourdelle <j.bourdelle@laposte.net>
2018-03-12 17:36:49 +0100

    DialogueBox Ok

---
+++ Assets/Scripts/OnMarkerFoundFunction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMarkerFoundFunction : MonoBehaviour {
+
+    public bool dialogueStarted = false;
+
+    public GameObject canvas;
+
+    void OnMarkerFound(ARMarker marker)
+    {
+        Debug.Log("MARKER "+ marker.Tag +" FOUND! WHEEEE!");
+
+        switch (marker.Tag)
+        {
+            case "peluchonMarker":
+                if (!dialogueStarted)
+                {
+                canvas.SetActive(true);
+                }
+                dialogueStarted = true;
+                break;
+        }
+        
+    }
+
+    void OnMarkerLost(ARMarker marker)
+    {
+        Debug.Log("MARKER LOST! WHEEEE!");
+    }
+
+    void OnMarkerTracked(ARMarker marker)
+    {
+        Debug.Log("MARKER Tracked ! WHEEEE!");
+    }
+}
